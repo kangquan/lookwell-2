8b1762f67f5fc6887acb170a826eea7db240bf54
Mhr <vipMhr@163.com>
2017-08-04 10:21:56 +0800

    项目初始化

---
+++ src/main/java/cn/bdqn/tangcco/controller/delete.java
@@ -0,0 +1,7 @@
+package cn.bdqn.tangcco.controller;
+
+/**
+ * Created by Administrator on 2017/8/4.
+ */
+public class delete {
+}
---
+++ src/main/java/cn/bdqn/tangcco/service/impl/delete.java
@@ -0,0 +1,7 @@
+package cn.bdqn.tangcco.service.impl;
+
+/**
+ * Created by Administrator on 2017/8/4.
+ */
+public class delete {
+}
